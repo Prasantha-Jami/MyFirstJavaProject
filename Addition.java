c5765752e5adab66ba6025be7da89f7db8cd6bf7
Prasantha-Jami <prasantha1508@gmail.com>
2022-11-24 22:28:30 +0530

    This is sample demo program

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package Assignment1;
+
+public class Addition {
+	public static void main(String[] args) {
+		int fnum=56;
+		int snum=50;
+				int result=fnum+snum;
+		System.out.println("Sum of two numbers:"+result);
+	}
+
+}
